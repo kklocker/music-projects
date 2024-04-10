783c6ad89ad0beafde0848d4de4a5439f2ba7a36
Karl Kristian Lockert <karladlock@gmail.com>
2024-04-10 21:49:05 +0200

    total sequence

--- drop-voicing-arpeggios/main.py
+++ drop-voicing-arpeggios/main.py
@@ -1,3 +1,12 @@
+from source.chord import (
+    adjustOctaveForAscendingChord,
+    getDrop2,
+    getDrop3,
+    getFillerNote,
+    shiftForInversion,
+)
+
+
 n_tones = 12  #  Assuming western 12-tone notes
 
 # TODO: Take as input
@@ -14,26 +23,69 @@ scale = [
 assert max(scale) < n_tones and min(scale) > 0
 
 # TODO: Take as  input
-root = 1 # 1-12, 1 == C
+root = 1  # 1-12, 1 == C
 
 scale_length = len(scale)
 
-
 # Generate chords from each note in the scale by stacking "thirds"
-chord_stacking_increment = 2 # thirds
-for (idx, root_note) in enumerate(scale):
-    scale_chord_for_root = []
+chord_stacking_increment = 2  # thirds
 
-    increment = 0
-    for i in range(4):
-        scale_chord_for_root.append(scale[(idx + increment)% scale_length])
-        increment += chord_stacking_increment
-    
-    print(scale_chord_for_root)
+inversion = 0  # 0..3; Four different ways to play the chord
 
-    drop_2 = [scale_chord_for_root[2], scale_chord_for_root[0], scale_chord_for_root[1], scale_chord_for_root[3]]
-    drop_3 = [scale_chord_for_root[1], scale_chord_for_root[0], scale_chord_for_root[2], scale_chord_for_root[3]]
-    print(f"Drop 2: {drop_2}")
-    print(f"Drop 3: {drop_3}")
+if __name__ == "__main__":
+    for idx, root_note in enumerate(scale):
+        scale_chord_for_root = []
 
+        increment = 0
+        for i in range(4):
+            scale_chord_for_root.append(scale[(idx + increment) % scale_length])
+            increment += chord_stacking_increment
 
+        # print(f"Scale chord: {scale_chord_for_root}")
+        adjusted_scalechord = adjustOctaveForAscendingChord(
+            scale_chord_for_root, n_tones
+        )
+        # print(f"Adjusted: {adjusted_scalechord}")
+        # print()
+        inversion_note = scale_chord_for_root[inversion]
+
+        drop_2 = getDrop2(scale_chord_for_root)
+        drop_2 = shiftForInversion(drop_2, inversion_note)
+        # print(f"Drop 2: {drop_2}")
+        adjusted_drop_2 = adjustOctaveForAscendingChord(drop_2, n_tones)
+        # print(f"Adjusted: {adjusted_drop_2}")
+        # print()
+        drop_3 = getDrop3(scale_chord_for_root)
+        drop_3 = shiftForInversion(drop_3, inversion_note)
+        adjusted_drop_3 = adjustOctaveForAscendingChord(drop_3, n_tones)
+
+        # print(f"Drop 3: {drop_3}")
+        # print(f"Adjusted: {adjusted_drop_3}")
+        # print()
+
+        # sequence: drop 2 asc + filler note + drop 3 asc + filler note(s) + closed form desc
+
+        firstFillerNote = getFillerNote(
+            adjusted_drop_2, adjusted_drop_3, scale, n_tones
+        )
+
+        # print(f"Filler note: {firstFillerNote}")
+
+        secondFillerNote = getFillerNote(
+            adjusted_drop_3, adjusted_scalechord[::-1], scale, n_tones
+        )
+
+        # print(f"Second filler note: {secondFillerNote}")
+
+        print("Total sequence: ")
+
+        print(
+            adjusted_drop_2,
+            firstFillerNote,
+            adjusted_drop_3,
+            secondFillerNote,
+            adjusted_scalechord[::-1],
+        )
+
+        print()
+        print()
---
+++ drop-voicing-arpeggios/source/chord.py
@@ -0,0 +1,42 @@
+from source.arr import shift
+
+
+def getDrop2(chord: list[int]) -> list[int]:
+    assert len(chord) == 4
+    return [chord[2], chord[0], chord[1], chord[3]]
+
+
+def getDrop3(chord: list[int]) -> list[int]:
+    assert len(chord) == 4
+    return [chord[1], chord[0], chord[2], chord[3]]
+
+
+def shiftForInversion(chord: list[int], inversion_note: int) -> list[int]:
+    index = chord.index(inversion_note)
+    return shift(chord, index)
+
+
+def adjustOctaveForAscendingChord(chord: list[int], n_tones: int) -> list[int]:
+    prev = 0
+    res = []
+    for current in chord:
+        while current < prev:
+            current += n_tones - 1
+
+        prev = current
+        res.append(current)
+
+    return res
+
+
+def getFillerNote(
+    firstChord: list[int], secondChord: list[int], scale: list[int], n_tones: int
+) -> int:
+    lastInFirst = firstChord[-1]
+    firstInLast = secondChord[0]
+    avg = (lastInFirst + firstInLast) // 2
+    scale_note = avg % (n_tones - 1)
+    octave = avg // n_tones
+    closest_scale_tone = min(scale, key=lambda x: abs(x - scale_note))
+    adjusted_to_octave_scaletone = closest_scale_tone + (n_tones - 1) * octave
+    return adjusted_to_octave_scaletone
